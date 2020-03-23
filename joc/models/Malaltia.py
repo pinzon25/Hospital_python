c2166f8aa4526cd8e57a6a0cc5206498f769dccf
Xavi Dobon <x.dobon10@gmail.com>
2020-03-23 12:31:43 +0100

    Parte xavi

--- joc/models/Malaltia.py
+++ joc/models/Malaltia.py
@@ -1,14 +1,21 @@
+from odoo import models, fields, api
 class Malaltia:
-	def __init__(self, codi, nom, causaBaixa, tractament, duradaTractament):
-        self.codi = codi
+	def __init__(self, codi, nom, causaBaixa, tractament, duradaTractament):self.codi = codi
         self.nom = nom
         self.causaBaixa = causaBaixa
         self.tractament = tractament
         self.duradaTractament = duradaTractament
+        def retornaCodiMalaltia(self):
+            return codi
+
+        def retornaNomMalaltia(self):
+            return nom
         
-        def print_malaltia(self):
-        print (self.codi)
-        print (self.nom)
-        print (self.causaBaixa)
-        print (self.tractament)
-        print (self.duradaTractament)
+        def retornacausaBaixaMalaltia(self):
+            return causaBaixa
+
+        def retornaTractamentMalaltia(self):
+            return tractament
+
+        def retornaduradaTractamentMalaltia(self):
+            return duradaTractament
