1345429daf8700b4d0e5aee5fa50041f4a86836f
fsarrate1 <fsarratepresas@gmail.com>
2020-04-23 22:05:13 +0200

    OK

--- gestiohospital/models/model_hospital.py
+++ gestiohospital/models/model_hospital.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api
 from . import model_persona
 class Hospital(models.Model):
     _name='hospitalprincipal'
-    _inherit = 'adreca.hospital'
 
     nomHospital=fields.Char(
         string='Nom del Hospital',
--- gestiohospital/models/model_pacient.py
+++ gestiohospital/models/model_pacient.py
@@ -2,7 +2,6 @@ from odoo import models, fields, api
 from . import model_persona
 class Pacienthospital(model_persona.Persona):
     _name='pacient.hospital'
-    _inherit = 'adreca.hospital'
 
     _historial=fields.Integer(
     string='Historial',
